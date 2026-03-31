85b251a967d1890fddceda3fa4d351b3b5571e87
unknown <iaraamancio1986@gmail.com>
2026-03-31 00:52:26 -0300

    adicionando bg e foto nna home

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,35 @@
 
 import './App.css'
 import { Header } from './components/header'
-import FotoLaura from './assets/laura01.png'
-import FotoLaura2 from './assets/laura02.jpeg'
+import FotoLaura from './assets/laura02.jpeg'
+import FotoLaura2 from './assets/laura05.png'
 
 function App() {
 
   return (
     <>
      <Header/>
-     <main className='w-full'>
-      <section className='w-full max-w-5xl mx-auto h-[calc(100vh-6rem)] flex md:flex-row flex-col px-2 gap-4'>
-        <div className='flex flex-1 flex-col justify-center'>
-          <div className=''>
+     <main className='w-full bg-zinc-50/50'>
+      <section className="w-full mx-auto h-[calc(100vh-6rem)] bg-[url('./assets/bg.png')] bg-cover bg-center  flex md:flex-row flex-col md:gap-4"
+      >      
+        <div className='flex flex-1 flex-col justify-center px-4'>
+          <div className='w-full md:max-w-10/12 mx-auto'>
+
               <h2 className='text-4xl font-semibold text-gold-primary'>Contadora</h2>
-              <h1 className='text-7xl font-bold text-gold-primary mb-2 -mt-3'>Laura Portela</h1>
+              <h1 className='md:text-7xl text-5xl font-bold text-gold-primary mb-2 -mt-3'>Laura Portela</h1>
               <p className='text-gray-primary/90 text-xl'>
                Especialista em contabilidade para Pessoa Física e Jurídica.
               <br></br><br></br>Cuido da burocracia para você focar no seu negócio, com organização e segurança.
-              </p>
-          
-          </div>
-          
+              </p> 
+          </div>                  
         </div>
-        <img src={FotoLaura} className='flex-1 h-full object-cover'/>      
+        <img src={FotoLaura2} className='flex-1 h-full object-cover object-[center_10%]'/>      
       </section>
 
-      <section id='sobre' className='w-full relative bg-gold-primary mx-auto pb-8 pt-24'>
-        <div className="absolute top-0 left-0 w-full overflow-hidden leading-[0]">
-        <svg
-          className="relative block w-full h-25"
-          viewBox="0 0 1440 320"
-          preserveAspectRatio="none"
-        >
-          <path
-            fill="#ffffff"
-            d="M0,160 C240,80 480,240 720,160 C960,80 1200,240 1440,160 L1440,0 L0,0 Z"
-          ></path>
-        </svg>
-      </div>
-        <div className='w-full max-w-5xl mx-auto flex items-center gap-4'>
+      <section id='sobre' className='w-full bg-gold-primary mx-auto pb-8 pt-24'>
+        <div className='w-full max-w-5xl mx-auto flex md:flex-row flex-col items-center gap-4'>
           <div className='flex-1'>
-            <img src={FotoLaura2} className='rounded-md h-150 w-125 object-cover'/>            
+            <img src={FotoLaura} className='rounded-md h-150 w-125 object-cover'/>            
           </div>
 
           <div className='flex-1'>
