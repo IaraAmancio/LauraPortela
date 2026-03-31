7cf370b089fd4b6d140fa5c235cdbe5a71028d65
unknown <iaraamancio1986@gmail.com>
2026-03-30 23:53:30 -0300

    removendo botão da home

--- src/App.tsx
+++ src/App.tsx
@@ -12,17 +12,14 @@ function App() {
      <main className='w-full'>
       <section className='w-full max-w-5xl mx-auto h-[calc(100vh-6rem)] flex md:flex-row flex-col px-2 gap-4'>
         <div className='flex flex-1 flex-col justify-center'>
-          <div className='w-4/5'>
-            <h2 className='text-2xl font-bold text-gray-primary'>Contadora</h2>
-              <h1 className='text-6xl font-bold text-gold-primary mb-6'>Laura Portela</h1>
-              <p className='text-gray-primary/90'>
-                Especialista em contabilidade digital para Pessoa física e Jurídica.
-                Eu cuido da parte burocrática para você cuidar do seu negócio.
-                <br></br><br></br>Qualquer pessoa pode ter um negócio organizado só basta ter o contador certo.
+          <div className=''>
+              <h2 className='text-4xl font-semibold text-gold-primary'>Contadora</h2>
+              <h1 className='text-7xl font-bold text-gold-primary mb-2 -mt-3'>Laura Portela</h1>
+              <p className='text-gray-primary/90 text-xl'>
+               Especialista em contabilidade para Pessoa Física e Jurídica.
+              <br></br><br></br>Cuido da burocracia para você focar no seu negócio, com organização e segurança.
               </p>
-              <button className='w-full mt-4 bg-gold-primary text-white rounded-md h-9'>
-                SOLICITAR CONSULTORIA
-              </button>
+          
           </div>
           
         </div>
